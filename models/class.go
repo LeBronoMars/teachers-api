143b48075021a0307dafae55ae38cc7cb59a33d1
Russel Bulanon <rbulanon@exist.com>
2016-11-16 18:46:21 +0800

    changed data type of grade level from int to string

--- models/class.go
+++ models/class.go
@@ -4,7 +4,7 @@ import "time"
 
 type Class struct {
 	BaseModel
-	GradeLevel int `json:"grade_level" form:"grade_level" binding:"required"`
+	GradeLevel string `json:"grade_level" form:"grade_level" binding:"required"`
 	Section string `json:"section" form:"section" binding:"required"`
 	SchoolYear int `json:"school_year"`
 	SchoolId int `json:"school_id" form:"school_id" binding:"required"`
