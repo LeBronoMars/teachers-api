2fd24b8a6415fe9e4a8fe6a15b4d6092eade1a99
Russel Bulanon <rbulanon@exist.com>
2016-11-24 21:43:32 +0800

    make subject code and name required

--- models/subject.go
+++ models/subject.go
@@ -2,8 +2,8 @@ package models
 
 type Subject struct {
 	BaseModel
-	SubjectName string `json:"subject_name" form:"subject_name"`
-	SubjectCode string `json:"subject_code" form:"subject_code"`
+	SubjectName string `json:"subject_name" form:"subject_name" binding:"required"`
+	SubjectCode string `json:"subject_code" form:"subject_code" binding:"required"`
 	Description string `json:"desciption" form:"desciption"`
 	IsSynced bool `json:"is_synched" form:"is_synched"`
 }
