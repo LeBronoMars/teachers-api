14d2ca509d84fd2db483f3b8fe590cb5cfa87b9f
Russel Bulanon <rbulanon@exist.com>
2016-11-27 06:49:16 +0800

    Fixed update profile pic and password

--- models/class.go
+++ models/class.go
@@ -1,20 +1,21 @@
 package models
 
-import "time"
+import "errors"
 
 type Class struct {
 	BaseModel
 	GradeLevel string `json:"grade_level" form:"grade_level" binding:"required"`
 	Section string `json:"section" form:"section" binding:"required"`
-	SchoolYearFrom int `json:"school_year_from"`
-	SchoolYearTo int `json:"school_year_to"`
+	SchoolYearFrom int `json:"school_year_from" form:"school_year_from" binding:"required"`
+	SchoolYearTo int `json:"school_year_to" form:"school_year_to" binding:"required"`
 	School string `json:"school_id" form:"school_id" binding:"required"`
 	Remarks string `json:"remarks" form:"remarks"`
 }
 
 func (c *Class) BeforeCreate() (err error) {
-	c.SchoolYearFrom = time.Now().Year()
-	c.SchoolYearTo = time.Now().Year() + 1
 	c.IsSynced = true
+	if c.SchoolYearFrom >= c.SchoolYearTo {
+		err = errors.New("School year to must be greater than school year from")
+	}
 	return
 }
--- server.go
+++ server.go
@@ -131,7 +131,7 @@ func InitDB() *gorm.DB {
 																&m.Student{},
 																&m.Subject{},
 																&m.ClassSubject{})
-	return &_db
+	return _db
 }
 
 func InitPusher() *pusher.Client {
