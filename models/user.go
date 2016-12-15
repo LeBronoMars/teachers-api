8b3eded9ae0ae907279498630369c19f911ea07b
Russel Bulanon <rbulanon@exist.com>
2016-12-16 00:16:27 +0800

    fixed issues in password encryption

--- models/user.go
+++ models/user.go
@@ -3,6 +3,11 @@ package models
 import (
 	"crypto/md5"
 	"fmt"
+	"io"	
+	"crypto/aes"	
+    "crypto/cipher"
+    "crypto/rand"
+    "encoding/base64"
 )
 
 type User struct {
@@ -34,7 +39,33 @@ func (u *User) BeforeCreate() (err error) {
 	return
 }
 
-func (u *User) AfterUpdate() (err error) {
+func (u *User) BeforeUpdate() (err error) {
 	u.IsSynced = true
+	u.Password = encrypt([]byte("vz7oWXaUm691nvAwXJuvs9U6UM04ZZs0"), u.Password)
 	return
+}
+
+// encrypt string to base64 crypto using AES
+func encrypt(key []byte, text string) string {
+	// key := []byte(keyText)
+	plaintext := []byte(text)
+
+	block, err := aes.NewCipher(key)
+	if err != nil {
+		panic(err)
+	}
+
+	// The IV needs to be unique, but not secure. Therefore it's common to
+	// include it at the beginning of the ciphertext.
+	ciphertext := make([]byte, aes.BlockSize+len(plaintext))
+	iv := ciphertext[:aes.BlockSize]
+	if _, err := io.ReadFull(rand.Reader, iv); err != nil {
+		panic(err)
+	}
+
+	stream := cipher.NewCFBEncrypter(block, iv)
+	stream.XORKeyStream(ciphertext[aes.BlockSize:], plaintext)
+
+	// convert to base64
+	return base64.URLEncoding.EncodeToString(ciphertext)
 }
